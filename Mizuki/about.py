d4c285a9f3761feb64019e76e390f8df87213eb3
ZenitsuXD #M•R•T™ <85629049+ZenitsuXD@users.noreply.github.com>
2021-11-23 15:00:25 +0700

    Update!

---
+++ Mizuki/about.py
@@ -0,0 +1,16 @@
+__help__ = """
+*Hatsune Miku - A Powerful Telegram Group Manager 🎶
+
+Powerful Abilities*
+
+• Group Voice Chat Music Play ❤
+
+• File To Link and URL Upload 📂
+
+• Youtube Downloader 🎵
+
+Developer @ZenitsuID 🇲🇨
+
+Github [ZenitsuXD](https://github.com/ZenitsuXD)"""
+
+__mod_name__ = "About 🇲🇨"
